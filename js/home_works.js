c246d6da98c5bacbef069c348cdf4203010f170e
luiskin27 <arstanbekova_l@iuca.kg>
2025-10-19 22:35:06 +0600

    Initial commit: Add lab files for phone and IIN checkers

--- js/home_works.js
+++ js/home_works.js
@@ -1,17 +1,35 @@
-const input = document.getElementById('gmail_input');
-const button = document.getElementById('gmail_button');
-const result = document.getElementById('gmail_result');
+const gmailInput = document.getElementById('gmail_input');
+const gmailButton = document.getElementById('gmail_button');
+const gmailResult = document.getElementById('gmail_result');
+
 
 const gmailRegExp = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
 
-button.addEventListener('click', ()=>{
-    const value = input.value.trim();
+gmailButton.addEventListener('click', ()=>{
+    const value = gmailInput.value.trim();
     if (gmailRegExp.test(value)){
-        result.textContent = 'Почта верна';
-        result.style.color = 'green';
+        gmailResult.textContent = 'Почта верна';
+        gmailResult.style.color = 'green';
     }else{
-         result.textContent = 'Почта не верна';
-         result.style.color = 'red';
+         gmailResult.textContent = 'Почта не верна';
+         gmailResult.style.color = 'red';
+    }
+});
+
+
+const iinInput = document.querySelector('#iin_input');
+const iinButton = document.querySelector('#iin_button');
+const iinResult = document.querySelector('#iin_result');
+
+const iinRegExp = /^\d{12}$/;
+
+iinButton.addEventListener('click', () => {
+    if (iinRegExp.test(iinInput.value)) {
+        iinResult.innerHTML = 'ИИН верный';
+        iinResult.style.color = 'green';
+    } else {
+        iinResult.innerHTML = 'ИИН неверный';
+        iinResult.style.color = 'red';
     }
 });
--- js/lesson.js
+++ js/lesson.js
@@ -5,10 +5,10 @@ const phoneSpan = document.querySelector('#phone_result');
 
 
 //+996550644772
-const reqExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
+const kgPhoneReqExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
 
 phoneButton.addEventListener('click', ()=>{
-    if (reqExp.test(phoneInput.value)){
+    if (kgPhoneReqExp.test(phoneInput.value)){
         phoneSpan.innerHTML = 'Этот номер существует';
         phoneSpan.style.color = 'green';
     }else {
@@ -17,4 +17,19 @@ phoneButton.addEventListener('click', ()=>{
     }
 })
 
+const phoneInputRu = document.querySelector('#phone_input_ru');
+const phoneButtonRu = document.querySelector('#phone_button_ru');
+const phoneSpanRu = document.querySelector('#phone_result_ru');
+
+const RussianPhoneReqExp = /^\+7 [9]\d{2} \d{3}-\d{2}-\d{2}$/
+
+phoneButtonRu.addEventListener('click', ()=>{
+    if (RussianPhoneReqExp.test(phoneInputRu.value)){
+        phoneSpanRu.innerHTML = 'Этот номер существует';
+        phoneSpanRu.style.color = 'green';
+    }else {
+        phoneSpanRu.innerHTML = 'Этот номер не существует';
+        phoneSpanRu.style.color = 'red';
+    }
+})
 
